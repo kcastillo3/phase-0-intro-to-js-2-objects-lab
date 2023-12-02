a903c58087eacf94e82a7b0ad088142e2e490795
Kevin <kevincastillo333@gmail.com>
2023-12-02 00:19:33 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+// Step 1: Initialize the employee object
+let employee = {
+  name: "John Doe",
+  streetAddress: "123 Main St"
+};
+
+// Step 2: Create the functions
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  let newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+// Step 3: Test the functions
+console.log(updateEmployeeWithKeyAndValue(employee, "name", "Jane Smith"));
+console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "456 Elm St"));
+console.log(deleteFromEmployeeByKey(employee, "name"));
+console.log(destructivelyDeleteFromEmployeeByKey(employee, "streetAddress"));
